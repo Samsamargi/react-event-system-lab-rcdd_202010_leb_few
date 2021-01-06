1fe6d420a166a9060234083ee0299eb2d85159a5
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2021-01-06 10:43:12 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+
+class Keypad extends React.Component{
+  keypad = () => {
+    console.log('Entering password...');
+  }
+  render(){
+    return(
+      <input type="password "onKeyUp = {this.keypad} >Password/>);
+  }
+}
